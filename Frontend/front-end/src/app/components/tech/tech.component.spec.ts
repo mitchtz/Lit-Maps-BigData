6b8ce518f651393ce5e37fb3d7c5ee937bf4c2fb
Nolawee Mengist <nome9478@colorado.edu>
2017-04-19 17:08:18 -0600

    jumbotron componnent made along with setting up routes

---
+++ Frontend/front-end/src/app/components/tech/tech.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { TechComponent } from './tech.component';
+
+describe('TechComponent', () => {
+  let component: TechComponent;
+  let fixture: ComponentFixture<TechComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TechComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TechComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Frontend/front-end/src/app/components/tech/tech.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tech',
+  templateUrl: './tech.component.html',
+  styleUrls: ['./tech.component.css']
+})
+export class TechComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
