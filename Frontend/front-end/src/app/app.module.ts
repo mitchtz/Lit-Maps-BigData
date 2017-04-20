7fe3c4189266c403141650ecb11851ef79e6aced
Nolawee Mengist <nome9478@colorado.edu>
2017-04-19 20:43:35 -0600

    added jumbotron

--- Frontend/front-end/src/app/app.module.ts
+++ Frontend/front-end/src/app/app.module.ts
@@ -2,23 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar/navbar.component';
 import { AboutComponent } from './components/about/about.component';
 import { CardComponent } from './components/card/card.component';
+import { MapComponent } from './components/map/map.component';
+import { TechComponent } from './components/tech/tech.component';
+import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
+
+const appRoutes: Routes = [
+    {path:'', component: CardComponent},
+    {path:'tech', component: TechComponent},
+    {path:'about', component: AboutComponent},
+    {path:'map', component: MapComponent}
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     AboutComponent,
-    CardComponent
+    CardComponent,
+    MapComponent,
+    TechComponent,
+    JumbotronComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
